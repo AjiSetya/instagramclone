8a1b259f03835b092e585b1f3ffefcbe01a3d0a2
AjiSetya <setyaaji07@gmail.com>
2019-03-18 14:49:13 +0700

    add UI item post

--- app/src/main/java/com/blogspot/blogsetyaaji/istagramclone/MainActivity.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/istagramclone/MainActivity.java
@@ -3,10 +3,9 @@ package com.blogspot.blogsetyaaji.istagramclone;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
